207662c2e20602d5a7a3d396b33452d70323e6ce
RC_Student_lab <RC_Student_lab@10.0.0.21>
2024-09-30 16:08:29 +0000

    POE

--- src/main/java/com/mycompany/poepart1/part1Class.java
+++ src/main/java/com/mycompany/poepart1/part1Class.java
@@ -9,5 +9,40 @@ package com.mycompany.poepart1;
  * @author RC_Student_lab
  */
 public class part1Class {
+     String enteredUsername ,enteredPassword;
+     String firstName,lastName, password,username;
+     
+      // Check if the username is correct
+    public boolean checkUsername() {
+        if (username.length() <= 5 && username.contains("_")) {
+            return true;
+        }
+        return false;
+    }
+     // Check the passwordcomplexity 
+    public boolean checkPasswordComplexity() {
+        boolean hasCapitalLetter = false;
+        boolean hasNumber = false;
+        boolean hasSpecialChar = false;
+
+        if (password.length() >= 8) {
+            for (char ch : password.toCharArray()) {
+                if (Character.isUpperCase(ch)) {
+                    hasCapitalLetter = true;
+                } else if (Character.isDigit(ch)) {
+                    hasNumber = true;
+                } else if (!Character.isLetterOrDigit(ch)) {
+                    hasSpecialChar = true;
+                }
+            }
+        }
+        return hasCapitalLetter && hasNumber && hasSpecialChar;
+    }
+
+
+      
+
+     
+     
     
 }
